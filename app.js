4551b9140fed86366dd8150c53d8c6e18cc7a08f
zacharybaca <zachary.baca@msn.com>
2024-08-12 08:01:40 -0500

    complete

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Middleware For Reading Requests From Body
+app.use(express.json());
+
+
+app.use('/bounties', require('./routes/bountyRouter'));
+
+
+// Starts The Server
+app.listen(3000, () => {
+    console.log('Server Listening on Port 3000')
+});
