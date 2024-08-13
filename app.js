69fc762984e77be13e38228d1da731917c5b972f
zacharybaca <zachary.baca@msn.com>
2024-08-12 21:53:22 -0500

    completed part 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 
-app.use('/bounties', require('./routes/bountyRouter'));
+app.use('/bounty', require('./routes/bountyRouter'));
 
 
 // Starts The Server
