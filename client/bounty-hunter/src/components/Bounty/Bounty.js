31ec5ce2a37bf319b7c5e5cbade77b2cd3f4be75
Zachary Baca <zachary.baca@msn.com>
2024-08-15 12:45:57 -0500

    Update Bounty.js

--- client/bounty-hunter/src/components/Bounty/Bounty.js
+++ client/bounty-hunter/src/components/Bounty/Bounty.js
@@ -1,14 +1,29 @@
+import React, { useState } from 'react';
+import Form from '../Form/Form';
 import "./bounty.css";
 
 
-export default function Bounty() {
+export default function Bounty(props) {
+    const [showForm, setShowForm] = useState(false);
+
+
     return (
         <div id="bounty-container">
             <ul>
-                <li>Test</li>
-                <li>Test</li>
-                <li>Test</li>
-                
+                {props.bounties.map((bounty) => {
+                    {showForm ? <><Form /> <button type="button" onClick={() => setShowForm(prevState => !prevState)}>Close</button></>: 
+                        <li key={bounty.id}>
+                            <>
+                            <h1>`${bounty.firstName} ${bounty.lastName}`</h1>
+                            <h3>`Living: ${bounty.living}`</h3>
+                            <h3>`Amount: ${bounty.bountyAmount}`</h3>
+                            <h3>`Type: ${bounty.type}`</h3>
+                            <button type="submit">Edit</button>
+                            <button type="button">Delete</button>
+                            </>    
+                    </li>
+                    }
+                })}
             </ul>
         </div>
     )
