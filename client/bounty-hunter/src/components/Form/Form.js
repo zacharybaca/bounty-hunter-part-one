90fd0a871d4a8ddc643bd0b83a839d7828cc5e48
Zachary Baca <zachary.baca@msn.com>
2024-08-15 09:40:25 -0500

    Update Form.js

--- client/bounty-hunter/src/components/Form/Form.js
+++ client/bounty-hunter/src/components/Form/Form.js
@@ -1,23 +1,42 @@
+import React, { useState } from 'react';
 import "./form.css";
 
 
 export default function Form() {
+    const initialInputs = {
+        firstName: "",
+        lastName: "",
+        living: false,
+        bountyAmount: "",
+        type: ""
+    }
+
+    const [inputs, setInputs] = useState(initialInputs);
+
+    function handleChange(e) {
+        const { name, value, type, checked } = e.target;
+
+        setInputs(prevState => ({
+            ...prevState,
+            [name]: type === "checkbox" ? checked : value
+        }))
+    }
     return (
         <div id="form-container">
             <form id="bounty-form" name="bountyForm">
                 <label htmlFor="firstName">Enter Bounty's First Name: </label>
-                <input type="text" id="firstName" name="firstName" placeholder="First Name" />
+                <input type="text" id="firstName" name="firstName" placeholder="First Name" value={inputs.firstName} onChange={handleChange}/>
                 <label htmlFor="lastName">Enter Bounty's Last Name: </label>
-                <input type="text" id="lastName" name="lastName" placeholder="Last Name" />
+                <input type="text" id="lastName" name="lastName" placeholder="Last Name" value={inputs.lastName} onChange={handleChange}/>
                 <label htmlFor="living" id="living-label">Is The Bounty Still Alive?: 
-                    <input type="checkbox" id="living" name="living" />
+                    <input type="checkbox" id="living" name="living" checked={inputs.living} onChange={handleChange}/>
                 </label>
                 <label htmlFor="bountyAmount">How Much Is The Bounty Worth?: </label>
-                <input type="number" id="bountyAmount" name="bountyAmount" placeholder="Bounty Amount" />
+                <input type="number" id="bountyAmount" name="bountyAmount" placeholder="Bounty Amount" value={inputs.bountyAmount} onChange={handleChange}/>
                 <label htmlFor="type">What Type Of Bounty Is The Target?</label>
-                <select id="type" name="type">
+                <select id="type" name="type" value={inputs.type} onChange={handleChange}>
                     <optgroup>
-                        <option selected value="">Select A Type</option>
+                        <option defaultValue value="">Select A Type</option>
                         <option value="sith">Sith</option>
                         <option value="jedi">Jedi</option>
                     </optgroup>
