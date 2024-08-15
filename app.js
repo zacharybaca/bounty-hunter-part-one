82637d5fb008b41849cf1891a751cb9f417d6334
Zachary Baca <zachary.baca@msn.com>
2024-08-15 14:28:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 
-app.use('/bounty', require('./routes/bountyRouter'));
+app.use('/api/bounty', require('./routes/bountyRouter'));
 
 
 // Starts The Server
