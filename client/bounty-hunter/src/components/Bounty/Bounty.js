bba6835177d9cd714fe82281e43aed32775d693e
Zachary Baca <zachary.baca@msn.com>
2024-08-15 13:58:49 -0500

    Update Bounty.js

--- client/bounty-hunter/src/components/Bounty/Bounty.js
+++ client/bounty-hunter/src/components/Bounty/Bounty.js
@@ -10,10 +10,10 @@ export default function Bounty(props) {
     return (
         <div id="bounty-container">
             {!showForm ? <>
-                <h1>`${props.firstName} ${props.lastName}`</h1>
-                <h3>`Living: ${props.living}`</h3>
-                <h3>`Amount: ${props.bountyAmount}`</h3>
-                <h3>`Type: ${props.type}`</h3>
+                <h1 id="name-heading">Name: {props.firstName} {props.lastName}</h1>
+                <h3>Living: {props.living}</h3>
+                <h3>`Amount: {props.bountyAmount}`</h3>
+                <h3>`Type: {props.type}`</h3>
                 <button type="button">Delete</button>
                 <button type="button" onClick={() => setShowForm(prevState => !prevState)}>Edit</button>
                 </> : <><Form bttnText="Submit Edit" firstName={props.firstName} lastName={props.lastName} living={props.living} bountyAmount={props.bountyAmount} type={props.type}/> <button type="button" onClick={() => setShowForm(prevState => !prevState)}>Close</button></>}   
