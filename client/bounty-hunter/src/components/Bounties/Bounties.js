2a7960ccc6aa65831d7d2025b8fe41ae01e5f226
Zachary Baca <zachary.baca@msn.com>
2024-08-15 15:17:40 -0500

    Update Bounties.js

--- client/bounty-hunter/src/components/Bounties/Bounties.js
+++ client/bounty-hunter/src/components/Bounties/Bounties.js
@@ -5,7 +5,19 @@ import Bounty from "../Bounty/Bounty";
 export default function Bounties(props) {
     return (
         <div id="bounties-container">
-            <Bounty bounties={props.bounties}/>
+            <ul>
+                {props.bounties.map((bounty) => {
+                    <li>
+                        <Bounty 
+                            firstName={bounty.firstName}
+                            lastName={bounty.lastName}
+                            living={bounty.living}
+                            bountyAmount={bounty.bountyAmount}
+                            type={bounty.type}
+                        />
+                    </li>
+                })}
+            </ul>
         </div>
     )
 }
