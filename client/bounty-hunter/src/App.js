427194c236732fbdf7f2f3d05301fab379bbccbc
Zachary Baca <zachary.baca@msn.com>
2024-08-15 12:48:37 -0500

    Update App.js

--- client/bounty-hunter/src/App.js
+++ client/bounty-hunter/src/App.js
@@ -1,13 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import Form from "./components/Form/Form";
 import Bounties from "./components/Bounties/Bounties";
 import './App.css';
 
 function App() {
+  const [bounties, setBounties] = useState([]);
+  
+
+  const getBounties = async () => {
+    const response = await fetch('/bounty')
+    const data = await response.json();
+
+    setBounties(data);
+  }
+
+  const addBounty = async (newBounty) => {
+    const response = await fetch('/bounty', {
+      method: "POST",
+      body: JSON.stringify(newBounty)
+    })
+    const data = await response.json();
+
+    setBounties(prevState => ({
+      ...prevState,
+      data
+    }))
+  }
+
+  useEffect(() => {
+    getBounties();
+  }, [])
+
   return (
     <div id="app-container">
-      <h1>Bounty Tracker Application</h1>
-      <Form />
-      <Bounties />
+      <h1>Bounty Hunter Tracker Application</h1>
+      <Form addBounty={addBounty}/>
+      <Bounties addBounty={addBounty} bounties={bounties}/>
     </div>
   );
 }
