3d18958a9e52cdffba399cb79c5cfda5b70d8a9f
Zachary Baca <zachary.baca@msn.com>
2024-08-16 08:27:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // Middleware For Reading Requests From Body
 app.use(express.json());
 
+app.use(morgan('combined'));
 
 app.use('/api/bounties', require('./routes/bountyRouter'));
 
