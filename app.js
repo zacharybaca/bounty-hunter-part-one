23995e28cde7ccb05ba68063fdc49408b7982e08
zacharybaca <zachary.baca@msn.com>
2024-08-20 12:40:36 -0500

    implemented mongodb with mongoose for db

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express();
 
 // Middleware For Reading Requests From Body
@@ -8,6 +10,18 @@ app.use(express.json());
 app.use(morgan('combined'));
 
 app.use('/api/bounties', require('./routes/bountyRouter'));
+
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
+};
+
+connectToMongoDB();
 
 
 // Starts The Server
